d030cec84f8280e8b3ee58666343701a98518e90
Tanzim Mokammel <mtanzim@gmail.com>
2025-03-29 14:18:38 -0400

    gather thread snippets for gmail

--- src/gmail-read.ts
+++ src/gmail-read.ts
@@ -8,34 +8,83 @@ import { authorize } from "./gmail-auth.js";
 
 async function listMessages(auth: Auth.OAuth2Client) {
   const gmail = google.gmail({ version: "v1", auth });
-  await getMessages(gmail);
+  const snippets = await getMessages(gmail);
+  let snippetsSerialized: Record<string, string> = {};
+  for (const [k, v] of snippets) {
+    snippetsSerialized[k] = v.join("\n");
+  }
+  console.log(JSON.stringify(snippetsSerialized, null, 2));
 }
 
 async function getMessages(
   gmail: gmail_v1.Gmail,
   pageToken?: string,
   page = 0
-) {
+): Promise<Map<string, string[]>> {
   console.log({ page });
   const res = await gmail.users.messages.list({
     userId: "me",
-    q: "newer_than:7d",
+    q: "newer_than:3d",
     pageToken: pageToken,
     maxResults: 10,
   });
-  processMessages(res.data?.messages || []);
+  const [snippets] = await processMessageThreads(
+    gmail,
+    res.data?.messages || []
+  );
   const nextToken = res.data.nextPageToken;
   if (nextToken) {
-    getMessages(gmail, nextToken, page + 1);
+    const res = await getMessages(gmail, nextToken, page + 1);
+    for (const [k, v] of res) {
+      snippets.set(k, v);
+    }
   }
+  return snippets;
 }
 
-function processMessages(messages: gmail_v1.Schema$Message[]): void {
+async function processMessageThreads(
+  gmail: gmail_v1.Gmail,
+  messages: gmail_v1.Schema$Message[]
+): Promise<[Map<string, string[]>, Map<string, gmail_v1.Schema$Message[]>]> {
   if (!messages || messages?.length === 0) {
     console.log("No labels found.");
-    return;
+    return [new Map(), new Map()];
   }
-  messages.forEach((m) => console.log(m.id));
+  const snippets = new Map<string, string[]>();
+  const fullMessages = new Map<string, gmail_v1.Schema$Message[]>();
+  // do it sequentially to avoid rate limiting for now
+  for (const m of messages) {
+    const fullMessageRes = await getMessage(gmail, m);
+    if (fullMessageRes.status !== 200) {
+      console.error(fullMessageRes.statusText);
+      continue;
+    }
+    const fullMessage = fullMessageRes.data;
+    if (!fullMessage.threadId) {
+      continue;
+    }
+    snippets.set(
+      fullMessage.threadId,
+      (snippets.get(fullMessage.threadId) || []).concat(
+        fullMessage.snippet || ""
+      )
+    );
+    fullMessages.set(
+      fullMessage.threadId,
+      (fullMessages.get(fullMessage.threadId) || []).concat(fullMessage) || []
+    );
+  }
+  return [snippets, fullMessages];
 }
 
-authorize().then(listMessages).catch(console.error);
+async function getMessage(gmail: gmail_v1.Gmail, m: gmail_v1.Schema$Message) {
+  return gmail.users.messages.get({
+    id: m.id || "",
+    userId: "me",
+    format: "full",
+  });
+}
+
+if (import.meta.url === `file://${process.argv[1]}`) {
+  authorize().then(listMessages).catch(console.error);
+}
