981f6e6e0bdcdbfaa25fdc5cfb74f4d4858ada3f
Tanzim Mokammel <mtanzim@gmail.com>
2025-04-19 13:54:34 -0400

    fix issues with snippet parsing

--- src/gmail-read.ts
+++ src/gmail-read.ts
@@ -1,22 +1,17 @@
-import {
-  google, // The top level object used to access services
-  Auth, // Namespace for auth related types
-  gmail_v1,
-} from "googleapis";
+import { Auth, gmail_v1, google } from "googleapis";
 
 import { authorize } from "./gmail-auth.js";
-import { threadId } from "worker_threads";
 
 export async function listThreadSnippets(
   auth: Auth.OAuth2Client
 ): Promise<string> {
   const gmail = google.gmail({ version: "v1", auth });
   const snippets = await getThreadSnippets(gmail);
-  let snippetsSerialized: Record<string, string> = {};
-  for (const [k, v] of snippets) {
-    snippetsSerialized[k] = v.join("\n");
+  let snippetsSerialized: Record<string, string[]> = {};
+  for (const [k, v] of snippets.entries()) {
+    snippetsSerialized[k] = v;
   }
-  return Object.values(snippetsSerialized).join("\n\n\n");
+  return Object.values(snippetsSerialized).flat().join("\n\n\n");
 }
 
 async function getThreadSnippets(
@@ -24,7 +19,6 @@ async function getThreadSnippets(
   pageToken?: string,
   page = 0
 ): Promise<Map<string, string[]>> {
-  // console.log({ page });
   const res = await gmail.users.messages.list({
     userId: "me",
     q: "newer_than:3d",
@@ -35,6 +29,10 @@ async function getThreadSnippets(
     gmail,
     res.data?.messages || []
   );
+  // modify the final snippets
+  for (const [k, v] of snippets) {
+    snippets.set(k, parseSnippet(k, v, fullMessageRes));
+  }
   const nextToken = res.data.nextPageToken;
   if (nextToken) {
     const res = await getThreadSnippets(gmail, nextToken, page + 1);
@@ -42,40 +40,43 @@ async function getThreadSnippets(
       snippets.set(k, v);
     }
   }
-  // modify the final snippets
-  for (const [k, v] of snippets) {
-    const fullMessage = fullMessageRes.get(k);
-    const meta = {
-      senderAddress:
-        fullMessage
-          ?.at(-1)
-          ?.payload?.headers?.find((h) => h?.name?.startsWith("From"))
-          ?.value || "",
-      subject:
-        fullMessage
-          ?.at(-1)
-          ?.payload?.headers?.find((h) => h?.name?.startsWith("Subject"))
-          ?.value || "",
-      threadId: k,
-      messageId: fullMessage?.at(-1)?.id,
-    };
-    const snippetVals = v.map((vv) => {
-      return `
-      <body>
-      \`\`\`plaintext
-      ${vv}
-      \`\`\`
-      </body>
-      <metadata>
-      \`\`\`json
-      ${JSON.stringify(meta)}
-      \`\`\`
-      </metadata>
-      `;
-    });
-    snippets.set(k, snippetVals);
-  }
+
   return snippets;
+}
+
+function parseSnippet(
+  threadId: string,
+  bodies: string[],
+  fullMessageRes: Map<string, gmail_v1.Schema$Message[]>
+) {
+  const fullMessage = fullMessageRes.get(threadId);
+  const meta = {
+    senderAddress:
+      fullMessage
+        ?.at(-1)
+        ?.payload?.headers?.find((h) => h?.name?.startsWith("From"))?.value ||
+      "",
+    subject:
+      fullMessage
+        ?.at(-1)
+        ?.payload?.headers?.find((h) => h?.name?.startsWith("Subject"))
+        ?.value || "",
+    threadId,
+    messageId: fullMessage?.at(-1)?.id,
+  };
+  const snippetVals = bodies.filter(Boolean).map((vv) => {
+    return `<body>
+\`\`\`plaintext
+${vv}
+\`\`\`
+</body>
+<metadata>
+\`\`\`json
+${JSON.stringify(meta)}
+\`\`\`
+</metadata>`;
+  });
+  return snippetVals;
 }
 
 async function processMessageThreads(
@@ -83,12 +84,10 @@ async function processMessageThreads(
   messages: gmail_v1.Schema$Message[]
 ): Promise<[Map<string, string[]>, Map<string, gmail_v1.Schema$Message[]>]> {
   if (!messages || messages?.length === 0) {
-    // console.log("No labels found.");
     return [new Map(), new Map()];
   }
   const snippets = new Map<string, string[]>();
   const fullMessages = new Map<string, gmail_v1.Schema$Message[]>();
-  // do it sequentially to avoid rate limiting for now
   for (const m of messages) {
     const fullMessageRes = await getMessage(gmail, m);
     if (fullMessageRes.status !== 200) {
