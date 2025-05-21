c416a23ff399bb14a6daa55ae01e83118e059930
mtanzim <mtanzim@gmail.com>
2025-05-20 20:36:19 -0400

    use email of signed in user

--- src/gmail-compose.ts
+++ src/gmail-compose.ts
@@ -1,9 +1,4 @@
-import {
-  google, // The top level object used to access services
-  Auth, // Namespace for auth related types
-  gmail_v1,
-} from "googleapis";
-import { authorize } from "./gmail-auth.js";
+import { Auth, google } from "googleapis";
 
 export async function draftEmail(
   auth: Auth.OAuth2Client,
@@ -22,8 +17,11 @@ export async function draftEmail(
   }
 ): Promise<string> {
   const gmail = google.gmail({ version: "v1", auth });
+  const profile = await gmail.users.getProfile({ userId: "me" });
+  const email = profile.data.emailAddress;
+
   const rawMessage =
-    `From: Tanzim Mokammel<mtanzim@gmail.com>\r\n` +
+    `From: <${email}>\r\n` +
     `To: ${address}\r\n` +
     `Subject: Re: ${subject}\r\n` + // Consider dynamically setting the subject
     `Content-Type: text/plain; charset=utf-8\r\n` +
@@ -51,29 +49,4 @@ export async function draftEmail(
     return "Draft was created successfully.";
   }
   return "Something went wrong. Please try again.";
-}
-
-if (import.meta.url === `file://${process.argv[1]}`) {
-  authorize()
-    .then((auth) =>
-      draftEmail(auth, {
-        address: `cameron.m@careers.getrocket.io`,
-        content: `\
-Hi Cameron,
-
-Thank you for your understanding. I appreciate your willingness to stay in touch and reconnect at a later time. \
-My current commitments are keeping me quite busy, but I would definitely be interested in exploring opportunities at Flipp when my schedule allows. \
-Please feel free to reach out again in about 3 months, as my situation should be more flexible then. In the meantime, I'll make a note to contact you if anything changes on my end. \
-Thanks again for your consideration.
-
-Best regards,
-         
-Tanzim`,
-        subject: `Re: Tanzim - Optimizing greenfield applications at Flipp?`,
-        threadId: `195ecc627a885fbe`,
-        messageId: `19640629052f84fd`,
-      })
-    )
-    .catch(console.error)
-    .then(console.log);
 }
