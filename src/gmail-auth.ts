6c1d39a11200a2eb20c04600676613df0e044bc7
Tanzim Mokammel <mtanzim@gmail.com>
2025-08-17 13:44:13 -0400

    format and lint

--- src/gmail-auth.ts
+++ src/gmail-auth.ts
@@ -7,14 +7,14 @@ const { google } = require("googleapis");
 
 // If modifying these scopes, delete token.json.
 const SCOPES = [
-  "https://www.googleapis.com/auth/gmail.readonly",
-  "https://www.googleapis.com/auth/gmail.compose",
+	"https://www.googleapis.com/auth/gmail.readonly",
+	"https://www.googleapis.com/auth/gmail.compose",
 ];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = process.env?.["TOKEN_PATH"]
-const CREDENTIALS_PATH = process.env?.["CREDENTIALS_PATH"]
+const TOKEN_PATH = process.env?.["TOKEN_PATH"];
+const CREDENTIALS_PATH = process.env?.["CREDENTIALS_PATH"];
 
 /**
  * Reads previously authorized credentials from the save file.
@@ -22,14 +22,14 @@ const CREDENTIALS_PATH = process.env?.["CREDENTIALS_PATH"]
  * @return {Promise<OAuth2Client|null>}
  */
 async function loadSavedCredentialsIfExist() {
-  try {
-    const content = await fs.readFile(TOKEN_PATH);
-    const credentials = JSON.parse(content);
-    return google.auth.fromJSON(credentials);
-  } catch (err) {
-    // console.log(err);
-    return null;
-  }
+	try {
+		const content = await fs.readFile(TOKEN_PATH);
+		const credentials = JSON.parse(content);
+		return google.auth.fromJSON(credentials);
+	} catch (err) {
+		// console.log(err);
+		return null;
+	}
 }
 
 /**
@@ -39,18 +39,18 @@ async function loadSavedCredentialsIfExist() {
  * @return {Promise<void>}
  */
 async function saveCredentials(client: {
-  credentials: { refresh_token: any };
+	credentials: { refresh_token: any };
 }) {
-  const content = await fs.readFile(CREDENTIALS_PATH);
-  const keys = JSON.parse(content);
-  const key = keys.installed || keys.web;
-  const payload = JSON.stringify({
-    type: "authorized_user",
-    client_id: key.client_id,
-    client_secret: key.client_secret,
-    refresh_token: client.credentials.refresh_token,
-  });
-  await fs.writeFile(TOKEN_PATH, payload);
+	const content = await fs.readFile(CREDENTIALS_PATH);
+	const keys = JSON.parse(content);
+	const key = keys.installed || keys.web;
+	const payload = JSON.stringify({
+		type: "authorized_user",
+		client_id: key.client_id,
+		client_secret: key.client_secret,
+		refresh_token: client.credentials.refresh_token,
+	});
+	await fs.writeFile(TOKEN_PATH, payload);
 }
 
 /**
@@ -58,19 +58,19 @@ async function saveCredentials(client: {
  *
  */
 export async function authorize() {
-  let client = await loadSavedCredentialsIfExist();
-  if (client) {
-    return client;
-  }
-  client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH,
-  });
-  if (client.credentials) {
-    await saveCredentials(client);
-  }
-  console.log("Authed")
-  return client;
+	let client = await loadSavedCredentialsIfExist();
+	if (client) {
+		return client;
+	}
+	client = await authenticate({
+		scopes: SCOPES,
+		keyfilePath: CREDENTIALS_PATH,
+	});
+	if (client.credentials) {
+		await saveCredentials(client);
+	}
+	console.log("Authed");
+	return client;
 }
 
 /**
@@ -79,17 +79,17 @@ export async function authorize() {
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 async function listLabels(auth: any) {
-  const gmail = google.gmail({ version: "v1", auth });
-  const res = await gmail.users.labels.list({
-    userId: "me",
-  });
-  const labels = res.data.labels;
-  if (!labels || labels.length === 0) {
-    return [];
-  }
-  return labels;
+	const gmail = google.gmail({ version: "v1", auth });
+	const res = await gmail.users.labels.list({
+		userId: "me",
+	});
+	const labels = res.data.labels;
+	if (!labels || labels.length === 0) {
+		return [];
+	}
+	return labels;
 }
 
 if (import.meta.url === `file://${process.argv[1]}`) {
-  authorize().then(listLabels).then(console.log).catch(console.error);
+	authorize().then(listLabels).then(console.log).catch(console.error);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,159 +1,159 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import * as dotenv from "dotenv";
-import { z } from "zod";
-import { getTomorrowWeatherForecast } from "./tomorrow.js";
-import { authorize } from "./gmail-auth.js";
-import { listThreadSnippets } from "./gmail-read.js";
-import { draftEmail } from "./gmail-compose.js";
-import express, { Request, Response } from "express";
+import express, { type Request, type Response } from "express";
 import morgan from "morgan";
-import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+import { z } from "zod";
+import { authorize } from "./gmail-auth.js";
+import { draftEmail } from "./gmail-compose.js";
+import { listThreadSnippets } from "./gmail-read.js";
+import { getTomorrowWeatherForecast } from "./tomorrow.js";
 dotenv.config();
 // Create server instance
 const server = new McpServer({
-  name: "Tanzim's tools",
-  version: "1.0.0",
+	name: "Tanzim's tools",
+	version: "1.0.0",
 });
 
 server.tool(
-  "get-forecast",
-  "Get weather forecast for a location",
-  {
-    latitude: z.number().min(-90).max(90).describe("Latitude of the location"),
-    longitude: z
-      .number()
-      .min(-180)
-      .max(180)
-      .describe("Longitude of the location"),
-  },
-  async ({ latitude, longitude }) => {
-    let forecastText = "";
-    try {
-      forecastText = await getTomorrowWeatherForecast({
-        lat: latitude.toFixed(4),
-        long: longitude.toFixed(4),
-      });
-    } catch (err: unknown) {
-      console.error(err);
-      console.log({ latitude, longitude });
-      forecastText = "Something went wrong. Cannot get weather forecast.";
-      if (err instanceof Error) {
-        forecastText = `${forecastText} Error: ${err.message}`;
-      }
-    }
+	"get-forecast",
+	"Get weather forecast for a location",
+	{
+		latitude: z.number().min(-90).max(90).describe("Latitude of the location"),
+		longitude: z
+			.number()
+			.min(-180)
+			.max(180)
+			.describe("Longitude of the location"),
+	},
+	async ({ latitude, longitude }) => {
+		let forecastText = "";
+		try {
+			forecastText = await getTomorrowWeatherForecast({
+				lat: latitude.toFixed(4),
+				long: longitude.toFixed(4),
+			});
+		} catch (err: unknown) {
+			console.error(err);
+			console.log({ latitude, longitude });
+			forecastText = "Something went wrong. Cannot get weather forecast.";
+			if (err instanceof Error) {
+				forecastText = `${forecastText} Error: ${err.message}`;
+			}
+		}
 
-    return {
-      content: [
-        {
-          type: "text",
-          text: forecastText,
-        },
-      ],
-    };
-  }
+		return {
+			content: [
+				{
+					type: "text",
+					text: forecastText,
+				},
+			],
+		};
+	},
 );
 
 server.tool(
-  "gmail-snippets",
-  "Get my gmail snippets from threads from the last 3 days",
-  {
-    name: z.string().optional(),
-  },
-  async ({ name }) => {
-    let snippetText = "";
-    try {
-      snippetText = await authorize().then(listThreadSnippets);
-    } catch (err: unknown) {
-      console.error(err);
-      snippetText = "Something went wrong. Cannot get gmail message threads.";
-      if (err instanceof Error) {
-        snippetText = `${snippetText} Error: ${err.message}`;
-      }
-    }
+	"gmail-snippets",
+	"Get my gmail snippets from threads from the last 3 days",
+	{
+		name: z.string().optional(),
+	},
+	async ({ name }) => {
+		let snippetText = "";
+		try {
+			snippetText = await authorize().then(listThreadSnippets);
+		} catch (err: unknown) {
+			console.error(err);
+			snippetText = "Something went wrong. Cannot get gmail message threads.";
+			if (err instanceof Error) {
+				snippetText = `${snippetText} Error: ${err.message}`;
+			}
+		}
 
-    return {
-      content: [
-        {
-          type: "text",
-          text: snippetText,
-        },
-      ],
-    };
-  }
+		return {
+			content: [
+				{
+					type: "text",
+					text: snippetText,
+				},
+			],
+		};
+	},
 );
 
 server.tool(
-  "gmail-draft-response",
-  "Creates draft responses to specified messages in Gmail.",
-  {
-    address: z
-      .string()
-      .email()
-      .describe("the address of the sended of the original email"),
-    content: z.string().describe("the content of the response"),
-    threadId: z.string().describe("the threadId of the original message"),
-    messageId: z.string().describe("the messageId of the original message"),
-    subject: z.string().describe("the subject of the original message"),
-  },
-  async ({ address, content, threadId, messageId, subject }) => {
-    let draftResponse = "";
-    try {
-      draftResponse = await authorize().then((auth) =>
-        draftEmail(auth, { address, content, threadId, messageId, subject })
-      );
-    } catch (err: unknown) {
-      console.error(err);
-      draftResponse = "Something went wrong. Cannot get gmail message threads.";
-      if (err instanceof Error) {
-        draftResponse = `${draftResponse} Error: ${err.message}`;
-      }
-    }
+	"gmail-draft-response",
+	"Creates draft responses to specified messages in Gmail.",
+	{
+		address: z
+			.string()
+			.email()
+			.describe("the address of the sended of the original email"),
+		content: z.string().describe("the content of the response"),
+		threadId: z.string().describe("the threadId of the original message"),
+		messageId: z.string().describe("the messageId of the original message"),
+		subject: z.string().describe("the subject of the original message"),
+	},
+	async ({ address, content, threadId, messageId, subject }) => {
+		let draftResponse = "";
+		try {
+			draftResponse = await authorize().then((auth) =>
+				draftEmail(auth, { address, content, threadId, messageId, subject }),
+			);
+		} catch (err: unknown) {
+			console.error(err);
+			draftResponse = "Something went wrong. Cannot get gmail message threads.";
+			if (err instanceof Error) {
+				draftResponse = `${draftResponse} Error: ${err.message}`;
+			}
+		}
 
-    return {
-      content: [
-        {
-          type: "text",
-          text: draftResponse,
-        },
-      ],
-    };
-  }
+		return {
+			content: [
+				{
+					type: "text",
+					text: draftResponse,
+				},
+			],
+		};
+	},
 );
 
 async function main() {
-  const transport = new StdioServerTransport();
-  await server.connect(transport);
-  console.log("Tanzim's MCP Server running on stdio");
+	const transport = new StdioServerTransport();
+	await server.connect(transport);
+	console.log("Tanzim's MCP Server running on stdio");
 }
 
 function startSSEServer() {
-  const app = express();
-  app.use(morgan("tiny"));
-  let transport: SSEServerTransport | null = null;
+	const app = express();
+	app.use(morgan("tiny"));
+	let transport: SSEServerTransport | null = null;
 
-  app.get("/sse", (_req: Request, res: Response) => {
-    transport = new SSEServerTransport("/messages", res);
-    server.connect(transport);
-  });
+	app.get("/sse", (_req: Request, res: Response) => {
+		transport = new SSEServerTransport("/messages", res);
+		server.connect(transport);
+	});
 
-  app.post("/messages", (req: Request, res: Response) => {
-    if (transport) {
-      transport.handlePostMessage(req, res);
-    }
-  });
+	app.post("/messages", (req: Request, res: Response) => {
+		if (transport) {
+			transport.handlePostMessage(req, res);
+		}
+	});
 
-  const port = 5222;
-  app.listen(port, () => {
-    console.log(`MCP SSE app listening on port ${port}`);
-  });
+	const port = 5222;
+	app.listen(port, () => {
+		console.log(`MCP SSE app listening on port ${port}`);
+	});
 }
 
 if (process.env?.MCP_SSE === "1") {
-  startSSEServer();
+	startSSEServer();
 } else {
-  main().catch((error) => {
-    console.error("Fatal error in main():", error);
-    process.exit(1);
-  });
+	main().catch((error) => {
+		console.error("Fatal error in main():", error);
+		process.exit(1);
+	});
 }
