3c7e6df5238e2001b58d0183ccc9e2946a4de39a
mtanzim <mtanzim@gmail.com>
2025-09-09 20:01:00 -0400

    add button to view more

--- src/mcp-ui-interfaces/gmail-thread.js
+++ src/mcp-ui-interfaces/gmail-thread.js
@@ -3,6 +3,8 @@
   const p = document.createElement("north-text-p");
   const textArea = document.createElement("north-text-area");
   const button = document.createElement("north-button");
+  const buttonView = document.createElement("north-button");
+  buttonView.setAttribute("label", "View full message");
 
   header.setAttribute("content", `{{subject}}`);
   p.setAttribute("content", `{{body}}`);
@@ -34,7 +36,27 @@
   }
   form.addEventListener("submit", handleSubmit);
 
+  function handleViewMore() {
+    console.log(
+      "Viewing message for: {{threadId}} and message id: {{messageId}}"
+    );
+
+    const params = {
+      type: "tool",
+      payload: {
+        toolName: "gmail-thread-full",
+        params: {
+          threadId: "{{threadId}}",
+        },
+      },
+    };
+    console.log(params);
+    window.top.postMessage(params, "*");
+  }
+  buttonView.addEventListener("press", handleViewMore);
+
   root.appendChild(header);
   root.appendChild(p);
+  root.appendChild(buttonView);
   root.appendChild(form);
 })();
